bd52d0ea68d1368f58b2604697ef79727208a434
Gennadii Berezovskii <bergencroc@gmail.com>
2019-01-16 23:55:18 +0100

    Adds TangemKit pod through the main cocoapod repo

---
+++ Pods/TangemKit/TangemKit/TangemKit/Classes/Extentions/StringExtensions.swift
@@ -0,0 +1,11 @@
+//
+//  StringExtensions.swift
+//  Tangem
+//
+//  Created by Gennady Berezovsky on 03.10.18.
+//  Copyright © 2018 Smart Cash AG. All rights reserved.
+//
+
+import Foundation
+
+extension String: Error {}
