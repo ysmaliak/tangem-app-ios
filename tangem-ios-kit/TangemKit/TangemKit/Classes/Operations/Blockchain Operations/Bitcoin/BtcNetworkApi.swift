97e58ed86d5a648cf81ea73050e79b43eab83015
Alexander Osokin <aosokin@tangem.com>
2019-09-06 17:31:06 +0300

    IOS-41
    incomplete

---
+++ tangem-ios-kit/TangemKit/TangemKit/Classes/Operations/Blockchain Operations/Bitcoin/BtcNetworkApi.swift
@@ -0,0 +1,14 @@
+//
+//  BtcNetworkApi.swift
+//  TangemKit
+//
+//  Created by Alexander Osokin on 04/09/2019.
+//  Copyright © 2019 Smart Cash AG. All rights reserved.
+//
+
+import Foundation
+
+enum BtcBackend {
+    case blockcypher
+    case blockchainInfo
+}
