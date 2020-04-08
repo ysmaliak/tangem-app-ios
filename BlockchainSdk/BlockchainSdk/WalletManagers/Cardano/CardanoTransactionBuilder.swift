6560e98b3191577f8acc783feaf3371dad9998ac
Alexander Osokin <aosokin@tangem.com>
2020-04-08 19:51:23 +0300

    IOS-182
    cardano incomplete

---
+++ BlockchainSdk/BlockchainSdk/WalletManagers/Cardano/CardanoTransactionBuilder.swift
@@ -0,0 +1,13 @@
+//
+//  CardanoTransactionBuilder.swift
+//  BlockchainSdk
+//
+//  Created by Alexander Osokin on 08.04.2020.
+//  Copyright © 2020 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+class CardanoTransactionBuilder {
+    
+}
