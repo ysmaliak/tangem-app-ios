34fda50eec30f0fbfb8a493b594b635e6b40f528
Alexander Osokin <aosokin@tangem.com>
2020-07-20 10:42:18 +0300

    IOS-403 skeleton arch

---
+++ Tangem Tap/Tangem Tap/Read/ReadViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ReadViewModel.swift
+//  Tangem Tap
+//
+//  Created by Alexander Osokin on 18.07.2020.
+//  Copyright © 2020 Tangem AG. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+class ReadViewModel: ObservableObject {
+    
+}
