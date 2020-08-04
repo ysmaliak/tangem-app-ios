04f30593666a33525dd9cc4e68815783ee887ff8
Alexander Osokin <aosokin@tangem.com>
2020-08-04 12:22:57 +0300

    IOS-409 Read screen

---
+++ Tangem Tap/Tangem Tap/Common/Extensions/Color+.swift
@@ -0,0 +1,25 @@
+//
+//  Color+.swift
+//  Tangem Tap
+//
+//  Created by Alexander Osokin on 03.08.2020.
+//  Copyright © 2020 Tangem AG. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    @nonobjc static var tangemTapBlue: Color {
+        return Color("tangem_tap_blue")
+    }
+    @nonobjc static var tangemTapBlack: Color {
+        return Color("tangem_tap_black")
+    }
+    @nonobjc static var tangemTapGreen: Color {
+        return Color("tangem_tap_green")
+    }
+    @nonobjc static var tangemBg: Color {
+        return Color("tangem_bg")
+    }
+}
