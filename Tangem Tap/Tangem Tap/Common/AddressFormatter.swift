8108c68035e4503f5f25d38f499e6b7d71eb1e07
Alexander Osokin <aosokin@tangem.com>
2020-08-15 18:08:53 +0300

    IOS-426 Main screen UI

---
+++ Tangem Tap/Tangem Tap/Common/AddressFormatter.swift
@@ -0,0 +1,26 @@
+//
+//  AddressFormatter.swift
+//  Tangem Tap
+//
+//  Created by Alexander Osokin on 14.08.2020.
+//  Copyright © 2020 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+struct AddressFormatter {
+    let address: String
+
+    init(address: String) {
+        self.address = address
+    }
+    
+    func truncated(prefixLimit: Int = 7, suffixLimit: Int = 4, delimiter: String = "...") -> String {
+        if address.count <= prefixLimit + suffixLimit + delimiter.count {
+            return address
+        }
+        
+        return "\(address.prefix(prefixLimit))\(delimiter)\(address.suffix(suffixLimit))"
+    }
+    
+}
