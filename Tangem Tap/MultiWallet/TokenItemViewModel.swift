28bd2ac7c46d567ae052bcc58781f924cd526157
Alexander Osokin <aosokin@tangem.com>
2021-03-10 17:20:41 +0300

    IOS-707 cleaning

--- Tangem Tap/MultiWallet/TokenItemViewModel.swift
+++ Tangem Tap/MultiWallet/TokenItemViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  WalletItem.swift
+//  TokenItemViewModel.swift
 //  Tangem Tap
 //
 //  Created by Alexander Osokin on 27.02.2021.
@@ -11,7 +11,7 @@ import BlockchainSdk
 import TangemSdk
 import SwiftUI
 
-struct WalletItemViewModel: Identifiable {
+struct TokenItemViewModel: Identifiable {
     var id = UUID()
     let state: WalletModel.State
     let hasTransactionInProgress: Bool
@@ -21,8 +21,6 @@ struct WalletItemViewModel: Identifiable {
     let rate: String
     var amountType: Amount.AmountType = .coin
     let blockchain: Blockchain
-
-    static let `default` = WalletItemViewModel(id: UUID(), state: .created, hasTransactionInProgress: false, name: "", fiatBalance: "", balance: "", rate: "", amountType: .coin, blockchain: .bitcoin(testnet: false))
     
     var currencySymbol: String {
         if amountType == .coin {
@@ -33,16 +31,18 @@ struct WalletItemViewModel: Identifiable {
         return ""
     }
     
-    var walletItem: WalletItem {
+    var walletItem: TokenItem {
         if case let .token(token) = amountType {
             return .token(token)
         }
         
         return .blockchain(blockchain)
     }
+    
+    static let `default` = TokenItemViewModel(id: UUID(), state: .created, hasTransactionInProgress: false, name: "", fiatBalance: "", balance: "", rate: "", amountType: .coin, blockchain: .bitcoin(testnet: false))
 }
 
-extension WalletItemViewModel {
+extension TokenItemViewModel {
     init(from balanceViewModel: BalanceViewModel, rate: String, blockchain: Blockchain) {
         hasTransactionInProgress = balanceViewModel.hasTransactionInProgress
         state = balanceViewModel.state
@@ -72,56 +72,3 @@ extension WalletItemViewModel {
     }
 }
 
-enum WalletItem: Codable, Hashable {
-    case blockchain(Blockchain)
-    case token(Token)
-    
-    var blockchain: Blockchain? {
-        if case let .blockchain(blockchain) = self {
-            return blockchain
-        }
-        return nil
-    }
-    
-    var token: Token? {
-        if case let .token(token) = self {
-            return token
-        }
-        return nil
-    }
-    
-    @ViewBuilder var imageView: some View {
-        switch self {
-        case .token(let token):
-            CircleImageView(name: token.name, color: token.color)
-        case .blockchain(let blockchain):
-            if let image = blockchain.imageName {
-                Image(image)
-            } else {
-                CircleImageView(name: blockchain.displayName,
-                                color: Color.tangemTapGrayLight4)
-            }
-        }
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        if let blockchain = try? container.decode(Blockchain.self) {
-            self = .blockchain(blockchain)
-        } else if let token = try? container.decode(Token.self) {
-            self = .token(token)
-        } else {
-            throw BlockchainSdkError.decodingFailed
-        }
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        switch self {
-        case .blockchain(let blockhain):
-            try container.encode(blockhain)
-        case .token(let token):
-            try container.encode(token)
-        }
-    }
-}
