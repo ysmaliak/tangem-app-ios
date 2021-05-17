de7b6550d9234951fefd6dac8504a62bcc57c6d8
Andrew Son <ason@tangem.com>
2021-05-17 10:19:19 +0500

    IOS-811 Handling deeplinks and universal links. Update localizations

---
+++ Tangem Tap/Common/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Tangem Tap
+//
+//  Created by Andrew Son on 13/05/21.
+//  Copyright © 2021 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    static var tangemDomain: String { tangemDomainUrl.absoluteString }
+    static var tangemDomainUrl: URL { URL(string: "https://tangem.com")! }
+}
