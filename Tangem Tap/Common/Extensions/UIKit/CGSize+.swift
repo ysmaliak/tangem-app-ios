adc3337ad844c76258f1a1b6a2c2f4e3c52e3117
Andrew Son <ason@tangem.com>
2021-08-23 10:15:48 +0500

    IOS-983 Animated progress bar with circles

---
+++ Tangem Tap/Common/Extensions/UIKit/CGSize+.swift
@@ -0,0 +1,21 @@
+//
+//  CGSize+.swift
+//  Tangem Tap
+//
+//  Created by Andrew Son on 19.08.2021.
+//  Copyright © 2021 Tangem AG. All rights reserved.
+//
+
+import CoreGraphics
+
+public extension String.StringInterpolation {
+    mutating func appendInterpolation(_ value: CGSize) {
+        appendInterpolation(value.description)
+    }
+}
+
+extension CGSize: CustomStringConvertible {
+    public var description: String {
+        "w: \(width), h: \(height)"
+    }
+}
