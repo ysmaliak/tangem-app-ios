ed8169f566f8139daa89eeecd95d4e766a4d8ac4
Andrey Chukavin <megadestroyer007@gmail.com>
2022-02-10 10:24:08 +0400

    IOS-1383 Added shop container/order view

---
+++ Tangem/Shop/ShopOrderView.swift
@@ -0,0 +1,22 @@
+//
+//  ShopOrderView.swift
+//  Tangem
+//
+//  Created by Andrey Chukavin on 10.02.2022.
+//  Copyright © 2022 Tangem AG. All rights reserved.
+//
+
+import SwiftUI
+
+struct ShopOrderView: View {
+    let order: Order
+    
+    var body: some View {
+        VStack {
+            SheetDragHandler()
+            WebViewContainer(url: order.statusUrl, title: "Order")
+        }
+        .edgesIgnoringSafeArea(.all)
+        .navigationBarHidden(true)
+    }
+}
