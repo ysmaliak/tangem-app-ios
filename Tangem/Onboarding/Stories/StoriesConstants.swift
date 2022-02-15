9a49f48d89f84ebf1535ae69230af7d583a22d7f
Andrey Chukavin <megadestroyer007@gmail.com>
2022-02-15 17:30:34 +0400

    IOS-1352 Added extra top padding to the story titles

---
+++ Tangem/Onboarding/Stories/StoriesConstants.swift
@@ -0,0 +1,13 @@
+//
+//  StoriesConstants.swift
+//  Tangem
+//
+//  Created by Andrey Chukavin on 15.02.2022.
+//  Copyright © 2022 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+enum StoriesConstants {
+    static let titleExtraTopPadding: Double = 40
+}
