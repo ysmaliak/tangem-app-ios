3749d1d5908ae6f450a1fd8c9610e77029ea7bd6
Alexander Osokin <tureckiy@gmail.com>
2022-06-16 19:40:52 +0700

    IOS-1632 Refactor onboarding module

---
+++ Tangem/Modules/Onboarding/BaseModels/OnboardingTopupViewModelRoutable.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingTopupViewModelRoutable.swift
+//  Tangem
+//
+//  Created by Alexander Osokin on 16.06.2022.
+//  Copyright © 2022 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+protocol OnboardingTopupViewModelRoutable: AnyObject {
+    func openCryptoShop(at url: URL, closeUrl: String, action: @escaping () -> Void)
+}
