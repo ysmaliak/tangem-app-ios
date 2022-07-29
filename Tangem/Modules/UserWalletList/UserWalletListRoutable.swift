55ea60a6ac9bb4dc7dbd1d8d24d7e464e5d5d6bd
Andrey Chukavin <megadestroyer007@gmail.com>
2022-07-29 16:49:06 +0300

    IOS-1943 Initial user wallet list version

---
+++ Tangem/Modules/UserWalletList/UserWalletListRoutable.swift
@@ -0,0 +1,13 @@
+//
+//  UserWalletListRoutable.swift
+//  Tangem
+//
+//  Created by Andrey Chukavin on 29.07.2022.
+//  Copyright © 2022 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+protocol UserWalletListRoutable: AnyObject {
+
+}
