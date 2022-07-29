81a0bd59ea04fc0244edbcc4d61c013b91dca487
Sergey Balashov <Balashov.152@gmail.com>
2022-07-29 13:17:41 +0300

    IOS-1924 update Social Networks

---
+++ Tangem/Common/UI/Types/LanguageCode.swift
@@ -0,0 +1,14 @@
+//
+//  LanguageCode.swift
+//  Tangem
+//
+//  Created by Sergey Balashov on 29.07.2022.
+//  Copyright © 2022 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+enum LanguageCode: String {
+    case en
+    case ru
+}
