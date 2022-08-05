892a0c2712602e66fe0c80c6baa6d259a45dc2a7
Alexander Osokin <tureckiy@gmail.com>
2022-08-05 15:20:58 +0700

    IOS-1903 WIP configs

--- Tangem/App/Models/UserWallet/Implementations/GenericDemoConfig.swift
+++
@@ -1,9 +0,0 @@
-//
-//  GenericDemoConfig.swift
-//  Tangem
-//
-//  Created by Alexander Osokin on 04.08.2022.
-//  Copyright © 2022 Tangem AG. All rights reserved.
-//
-
-import Foundation
--- Tangem/App/Models/UserWallet/Implementations/NoteDemoConfig.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DemoConfig.swift
-//  Tangem
-//
-//  Created by Alexander Osokin on 03.08.2022.
-//  Copyright © 2022 Tangem AG. All rights reserved.
-//
-
-import Foundation
