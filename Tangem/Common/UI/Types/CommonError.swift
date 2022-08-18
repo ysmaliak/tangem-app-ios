df13c7cd08cd6226d319301cba5683b5abb112d0
Sergey Balashov <Balashov.152@gmail.com>
2022-08-18 10:45:25 +0300

    IOS-1783 create tokens, if first time

---
+++ Tangem/Common/UI/Types/CommonError.swift
@@ -0,0 +1,13 @@
+//
+//  CommonError.swift
+//  Tangem
+//
+//  Created by Sergey Balashov on 17.08.2022.
+//  Copyright © 2022 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+enum CommonError: Error {
+    case masterReleased
+}
