9148435390c667c7bb665d5a99b4643c12b56149
Alexander Osokin <tureckiy@gmail.com>
2022-10-04 11:40:06 +0500

    IOS-2088 Visa onboarding WIP

---
+++ Tangem/Modules/Onboarding/Views/Visa/KYCWaitingView.swift
@@ -0,0 +1,21 @@
+//
+//  KYCWaitingView.swift
+//  Tangem
+//
+//  Created by Alexander Osokin on 03.10.2022.
+//  Copyright © 2022 Tangem AG. All rights reserved.
+//
+
+import SwiftUI
+
+struct KYCWaitingView: View {
+    var body: some View {
+        Image(name: "success_waiting")
+    }
+}
+
+struct KYCWaitingView_Previews: PreviewProvider {
+    static var previews: some View {
+        KYCWaitingView()
+    }
+}
