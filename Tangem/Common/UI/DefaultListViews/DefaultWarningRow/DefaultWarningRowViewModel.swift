34e288763adec4555e0e0cd04a3dbfd626c53f8f
Sergey Balashov <Balashov.152@gmail.com>
2022-10-26 20:00:00 +0600

    IOS-2085 Work on pixel perfect

---
+++ Tangem/Common/UI/DefaultListViews/DefaultWarningRow/DefaultWarningRowViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  DefaultWarningRowViewModel.swift
+//  Tangem
+//
+//  Created by Sergey Balashov on 26.10.2022.
+//  Copyright © 2022 Tangem AG. All rights reserved.
+//
+
+import SwiftUI
+
+struct DefaultWarningRowViewModel {
+    let icon: Image
+    let title: String
+    let subtitle: String
+
+    let action: () -> ()
+}
+
+extension DefaultWarningRowViewModel: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(title)
+        hasher.combine(subtitle)
+    }
+
+    static func == (lhs: DefaultWarningRowViewModel, rhs: DefaultWarningRowViewModel) -> Bool {
+        lhs.hashValue == rhs.hashValue
+    }
+}
+
+extension DefaultWarningRowViewModel: Identifiable {
+    var id: Int { hashValue }
+}
