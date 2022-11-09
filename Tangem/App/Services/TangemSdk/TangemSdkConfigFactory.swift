51b675a6907874fc90b807d78b1e05ab12708ac6
Andrey Chukavin <megadestroyer007@gmail.com>
2022-11-09 14:48:51 +0500

    IOS-1943 Remove debug code

--- Tangem/App/Services/TangemSdk/TangemSdkConfigFactory.swift
+++ Tangem/App/Services/TangemSdk/TangemSdkConfigFactory.swift
@@ -25,30 +25,6 @@ struct TangemSdkConfigFactory {
         config.filter.issuerFilter = .deny(["TTM BANK"])
         config.allowUntrustedCards = true
         config.biometricsLocalizedReason = "biometry_touch_id_reason".localized
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        #warning("TODO TODO TODO TODO TODO")
-        config.attestationMode = .offline
-
         return config
     }
 }
