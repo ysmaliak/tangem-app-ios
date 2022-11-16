14bff87f31a7b0663d011b716fac52f23661e222
Sergey Balashov <Balashov.152@gmail.com>
2022-11-16 10:17:47 +0300

    IOS-2364 Almost success build

---
+++ TangemExchange/Models/ExchangeItems.swift
@@ -0,0 +1,15 @@
+//
+//  ExchangeItems.swift
+//  TangemExchange
+//
+//  Created by Sergey Balashov on 15.11.2022.
+//  Copyright © 2022 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+public struct ExchangeItems {
+    public let walletAddress: String
+    public let source: Currency
+    public let destination: Currency?
+}
