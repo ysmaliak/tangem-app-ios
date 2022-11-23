9280420ba9435fc8ac563ae795874b738b99cff9
Sergey Balashov <Balashov.152@gmail.com>
2022-11-23 17:30:14 +0300

    IOS-2364 work on Logger

---
+++ TangemExchange/Utils/Logger.swift
@@ -0,0 +1,17 @@
+//
+//  Logger.swift
+//  TangemExchange
+//
+//  Created by Sergey Balashov on 23.11.2022.
+//  Copyright © 2022 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+
+/// TODO: Move it to TangemFoundation
+enum Logger {
+    static func log(args: Any...) {
+        
+    }
+}
