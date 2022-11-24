d11d5fa7d3a9725ce4945453a692f5e7cc7f3141
Sergey Balashov <Balashov.152@gmail.com>
2022-11-24 19:34:09 +0300

    IOS-2364 Refactoring 2

---
+++ TangemExchange/Managers/ExchangeManagerDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  ExchangeManagerDelegate.swift
+//  TangemExchange
+//
+//  Created by Sergey Balashov on 24.11.2022.
+//  Copyright © 2022 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+public protocol ExchangeManagerDelegate: AnyObject {
+    func exchangeManagerDidUpdate(availabilityState: SwappingAvailabilityState)
+    func exchangeManagerDidUpdate(swappingModel: ExchangeSwapDataModel)
+}
