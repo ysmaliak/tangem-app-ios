8d0373ae6a59c93e3f6dfab9770a742a1750600d
Sergey Balashov <Balashov.152@gmail.com>
2022-12-12 13:59:35 +0300

    IOS-2344 rename UserCurrenciesProvider

--- Tangem/App/Services/Exchange/UserCurrenciesProvider.swift
+++ Tangem/App/Services/Exchange/UserCurrenciesProvider.swift
@@ -1,5 +1,5 @@
 //
-//  UserWalletListProvider.swift
+//  UserCurrenciesProvider.swift
 //  Tangem
 //
 //  Created by Sergey Balashov on 06.12.2022.
@@ -8,7 +8,7 @@
 
 import TangemExchange
 
-struct UserWalletListProvider {
+struct UserCurrenciesProvider {
     private let walletModel: WalletModel
 
     init(walletModel: WalletModel) {
@@ -18,7 +18,7 @@ struct UserWalletListProvider {
 
 // MARK: - UserCurrenciesProviding
 
-extension UserWalletListProvider: UserCurrenciesProviding {
+extension UserCurrenciesProvider: UserCurrenciesProviding {
     func addCurrencyInList(currency: Currency) {
         // TODO: add currency to user token list
     }
