e12f3aea9e8d0e920b219200b94d306ac20720f6
Sergey Balashov <Balashov.152@gmail.com>
2022-12-12 11:19:57 +0300

    IOS-2344 Fix review 2

---
+++ Tangem/Preview Content/Mocks/Exchange/UserCurrenciesProviderMock.swift
@@ -0,0 +1,15 @@
+//
+//  UserCurrenciesProviderMock.swift
+//  Tangem
+//
+//  Created by Sergey Balashov on 06.12.2022.
+//  Copyright © 2022 Tangem AG. All rights reserved.
+//
+
+import Foundation
+import TangemExchange
+
+struct UserCurrenciesProviderMock: UserCurrenciesProviding {
+    func getCurrencies(blockchain: ExchangeBlockchain) -> [Currency] { [.mock] }
+    func addCurrencyInList(currency: Currency) {}
+}
