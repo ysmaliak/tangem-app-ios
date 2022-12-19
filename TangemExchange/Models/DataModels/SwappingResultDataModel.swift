6c0491432e7fbdd15e96a8509ec93062dd594c01
Sergey Balashov <Balashov.152@gmail.com>
2022-12-19 19:07:49 +0300

    IOS-2356 Add show error for fee

--- TangemExchange/Models/DataModels/SwappingResultDataModel.swift
+++ TangemExchange/Models/DataModels/SwappingResultDataModel.swift
@@ -1,39 +1,23 @@
 //
-//  ExpectedSwappingResult.swift
+//  SwappingResultDataModel.swift
 //  TangemExchange
 //
-//  Created by Sergey Balashov on 12.12.2022.
+//  Created by Sergey Balashov on 19.12.2022.
 //  Copyright © 2022 Tangem AG. All rights reserved.
 //
 
 import Foundation
-
-public struct PreviewSwappingDataModel {
-    public let expectedAmount: Decimal
-    public let expectedFiatAmount: Decimal
-    public let isEnoughAmountForExchange: Bool
-
-    public init(
-        expectedAmount: Decimal,
-        expectedFiatAmount: Decimal,
-        isEnoughAmountForExchange: Bool
-    ) {
-        self.expectedAmount = expectedAmount
-        self.expectedFiatAmount = expectedFiatAmount
-        self.isEnoughAmountForExchange = isEnoughAmountForExchange
-    }
-}
 
 public struct SwappingResultDataModel {
     public let amount: Decimal
     public let fiatAmount: Decimal
     public let fee: Decimal
     public let fiatFee: Decimal
-    
+
     public let isEnoughAmountForExchange: Bool
     public let isEnoughAmountForFee: Bool
     public let isRequiredPermission: Bool
-    
+
     public init(
         amount: Decimal,
         fiatAmount: Decimal,
