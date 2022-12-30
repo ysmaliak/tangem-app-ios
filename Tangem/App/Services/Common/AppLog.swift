962c2dec9654e09eb107b2e5edaa09255f7b2f79
Alexander Osokin <tureckiy@gmail.com>
2022-12-30 14:36:18 +0500

    IOS-2802 Refactor logger

---
+++ Tangem/App/Services/Common/AppLog.swift
@@ -0,0 +1,12 @@
+//
+//  AppLog.swift
+//  Tangem
+//
+//  Created by Alexander Osokin on 30.12.2022.
+//  Copyright © 2022 Tangem AG. All rights reserved.
+//
+
+import Foundation
+import TangemSdk
+
+typealias AppLog = Log
