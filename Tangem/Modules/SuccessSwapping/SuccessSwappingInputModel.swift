ad3911c79754168583a8e4f9ffe27c03c221a78a
Sergey Balashov <Balashov.152@gmail.com>
2023-01-13 15:44:45 +0300

    IOS-2855 redesign success view

---
+++ Tangem/Modules/SuccessSwapping/SuccessSwappingInputModel.swift
@@ -0,0 +1,15 @@
+//
+//  SuccessSwappingInputModel.swift
+//  Tangem
+//
+//  Created by Sergey Balashov on 13.01.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+struct SuccessSwappingInputModel {
+    let sourceCurrencyAmount: CurrencyAmount
+    let resultCurrencyAmount: CurrencyAmount
+    let explorerURL: URL?
+}
