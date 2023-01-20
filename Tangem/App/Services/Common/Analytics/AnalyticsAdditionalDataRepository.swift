ff55b2d8ac03d85c86d05415e26e3db46b86fcba
Andrey Chukavin <megadestroyer007@gmail.com>
2023-01-20 15:31:22 +0200

    Refactoring the was card scans are recorded

---
+++ Tangem/App/Services/Common/Analytics/AnalyticsAdditionalDataRepository.swift
@@ -0,0 +1,15 @@
+//
+//  AnalyticsAdditionalDataRepository.swift
+//  Tangem
+//
+//  Created by Andrey Chukavin on 20.01.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+extension Analytics {
+    class AdditionalDataRepository {
+        var cardDidScanEvent: Analytics.Event?
+    }
+}
