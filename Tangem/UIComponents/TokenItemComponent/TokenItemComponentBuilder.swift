6eb7e1269de8e6006f4b1db2dfd7b2876ef235bb
Andrew Son <andoran90@gmail.com>
2023-04-28 11:12:13 +0500

    IOS-3230: New token item component, Loader text view component

---
+++ Tangem/UIComponents/TokenItemComponent/TokenItemComponentBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  TokenItemComponentBuilder.swift
+//  Tangem
+//
+//  Created by Andrew Son on 27/04/23.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
---
+++ Tangem/UIComponents/TokenItemComponent/TokenItemInfoProvider.swift
@@ -0,0 +1,9 @@
+//
+//  TokenItemInfoProvider.swift
+//  Tangem
+//
+//  Created by Andrew Son on 27/04/23.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
