54c746227b02111ac70468d4ca959d97ff8a7f70
Andrew Son <andoran90@gmail.com>
2023-04-28 11:27:56 +0500

    IOS-3230: Remove empty files

--- Tangem/UIComponents/TokenItemComponent/TokenItemComponentBuilder.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TokenItemComponentBuilder.swift
-//  Tangem
-//
-//  Created by Andrew Son on 27/04/23.
-//  Copyright © 2023 Tangem AG. All rights reserved.
-//
-
-import Foundation
--- Tangem/UIComponents/TokenItemComponent/TokenItemInfoProvider.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TokenItemInfoProvider.swift
-//  Tangem
-//
-//  Created by Andrew Son on 27/04/23.
-//  Copyright © 2023 Tangem AG. All rights reserved.
-//
-
-import Foundation
