cd816f9985746152548c9033d09be2316d8bf858
Skibin Alexander <skibinalexander@gmail.com>
2023-05-14 18:56:29 +0300

    IOS-3456 Add file view

---
+++ Tangem/UIComponents/TogglableGroupButtonWithIcon/TogglableGroupButtonWithIcon.swift
@@ -0,0 +1,9 @@
+//
+//  TogglableGroupButtonWithIcon.swift
+//  Tangem
+//
+//  Created by skibinalexander on 14.05.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
