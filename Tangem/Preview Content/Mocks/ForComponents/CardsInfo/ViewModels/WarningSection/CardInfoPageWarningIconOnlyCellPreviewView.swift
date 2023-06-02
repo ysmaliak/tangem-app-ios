9eb90f7a6149b63817d00b44cafb9f74a1e1331a
Andrey Fedorov <m3g0byt3@gmail.com>
2023-06-02 18:11:56 +0300

    IOS-3449: Total rehaul of previews for `CardsInfoPagerView`

---
+++ Tangem/Preview Content/Mocks/ForComponents/CardsInfo/ViewModels/WarningSection/CardInfoPageWarningIconOnlyCellPreviewView.swift
@@ -0,0 +1,29 @@
+//
+//  CardInfoPageWarningIconOnlyCellPreviewView.swift
+//  Tangem
+//
+//  Created by Andrey Fedorov on 02/06/23.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import SwiftUI
+
+struct CardInfoPageWarningIconOnlyCellPreviewView: View {
+    @ObservedObject var viewModel: CardInfoPageWarningIconOnlyCellPreviewViewModel
+
+    let contentColor: Color
+
+    var body: some View {
+        HStack {
+            if let icon = viewModel.icon {
+                Image(uiImage: icon)
+            }
+
+            Text("No title provided")
+                .foregroundColor(.gray)
+        }
+        .padding()
+        .infinityFrame(alignment: .topLeading)
+        .background(contentColor)
+    }
+}
