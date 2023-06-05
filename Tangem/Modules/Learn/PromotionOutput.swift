314828104e1b25ef8de2a9275cbd0ab4e1a8d7b6
Andrey Chukavin <megadestroyer007@gmail.com>
2023-06-05 13:51:19 +0400

    IOS-3702 Added promotion output

---
+++ Tangem/Modules/Learn/PromotionOutput.swift
@@ -0,0 +1,13 @@
+//
+//  PromotionOutput.swift
+//  Tangem
+//
+//  Created by Andrey Chukavin on 05.06.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+protocol PromotionOutput: AnyObject {
+    func startAwardProcess()
+}
