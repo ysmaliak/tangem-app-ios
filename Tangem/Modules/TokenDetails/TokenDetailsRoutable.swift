7acbf91b45e93ca0e9cb77ff6c8651fc3b3287f5
Andrew Son <andoran90@gmail.com>
2023-06-13 17:41:31 +0500

    IOS-3462: Token details module, toggle

---
+++ Tangem/Modules/TokenDetails/TokenDetailsRoutable.swift
@@ -0,0 +1,13 @@
+//
+//  TokenDetailsRoutable.swift
+//  Tangem
+//
+//  Created by Andrew Son on 09/06/23.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+protocol TokenDetailsRoutable: AnyObject {
+    func openReceiveScreen()
+}
