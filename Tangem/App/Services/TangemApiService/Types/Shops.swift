69e3be6cd8a331de9db75c50d3c93305e1bc75e6
Andrey Chukavin <megadestroyer007@gmail.com>
2023-06-14 13:23:59 +0400

    IOS-3837 Added new API for /shops

---
+++ Tangem/App/Services/TangemApiService/Types/Shops.swift
@@ -0,0 +1,13 @@
+//
+//  Shops.swift
+//  Tangem
+//
+//  Created by Andrey Chukavin on 14.06.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+struct ShopParameters: Decodable {
+    let canOrder: Bool
+}
