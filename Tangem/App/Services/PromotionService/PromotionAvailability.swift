2f850d836f8d32b5b1a29f78d50d1ad2518110c7
Andrey Chukavin <megadestroyer007@gmail.com>
2023-06-14 17:16:31 +0400

    IOS-3803 Fetching award amount from the backend

---
+++ Tangem/App/Services/PromotionService/PromotionAvailability.swift
@@ -0,0 +1,14 @@
+//
+//  PromotionAvailability.swift
+//  Tangem
+//
+//  Created by Andrey Chukavin on 14.06.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+struct PromotionAvailability {
+    let isAvailable: Bool
+    let awardAmount: Decimal
+}
