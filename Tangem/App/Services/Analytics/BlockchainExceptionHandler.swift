511f0bab9f3fd8f563a93c276c19c797ef5694dc
Skibin Alexander <skibinalexander@gmail.com>
2023-06-23 12:21:26 +0300

    IOS-3664 Add implementation blockchain sdk handler

---
+++ Tangem/App/Services/Analytics/BlockchainExceptionHandler.swift
@@ -0,0 +1,23 @@
+//
+//  BlockchainExceptionHandler.swift
+//  Tangem
+//
+//  Created by skibinalexander on 06.06.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+import BlockchainSdk
+import Firebase
+
+extension Analytics {
+    struct BlockchainExceptionHandler: ExceptionHandlerOutput {
+        func handleAPISwitch(currentHost: String, nextHost: String, message: String) {
+            Analytics.log(
+                event: .blockchainSdkException,
+                params: [.exceptionHost: currentHost, .selectedHost: nextHost, .errorDescription: message],
+                analyticsSystems: [.crashlytics]
+            )
+        }
+    }
+}
