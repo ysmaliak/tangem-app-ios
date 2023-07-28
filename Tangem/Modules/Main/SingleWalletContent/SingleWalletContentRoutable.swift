9a7d4cef83e623d0c998b23de94a41b71403333e
Andrew Son <andoran90@gmail.com>
2023-07-28 19:03:29 +0500

    IOS-3463: Main view module with page generation

---
+++ Tangem/Modules/Main/SingleWalletContent/SingleWalletContentRoutable.swift
@@ -0,0 +1,11 @@
+//
+//  SingleWalletContentRoutable.swift
+//  Tangem
+//
+//  Created by Andrew Son on 28/07/23.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+protocol SingleWalletContentRoutable: AnyObject {}
