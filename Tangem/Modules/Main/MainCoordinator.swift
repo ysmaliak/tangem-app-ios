d2793cef5bc60b92c4373c6675cddb5657a612eb
Andrew Son <andoran90@gmail.com>
2023-07-31 13:47:54 +0500

    IOS-3463: Fix imports

--- Tangem/Modules/Main/MainCoordinator.swift
+++ Tangem/Modules/Main/MainCoordinator.swift
@@ -5,6 +5,7 @@
 //  Created by Andrew Son on 28/07/23.
 //  Copyright © 2023 Tangem AG. All rights reserved.
 //
+
 import Foundation
 import Combine
 
--- Tangem/Modules/Main/MainViewModel.swift
+++ Tangem/Modules/Main/MainViewModel.swift
@@ -5,8 +5,9 @@
 //  Created by Andrew Son on 28/07/23.
 //  Copyright © 2023 Tangem AG. All rights reserved.
 //
+
+import Foundation
 import Combine
-import SwiftUI
 
 final class MainViewModel: ObservableObject {
     // MARK: - ViewState
