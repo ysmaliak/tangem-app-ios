d92a63744f4c187e60beef4a4a37a4e9eb17df47
Andrey Fedorov <m3g0byt3@gmail.com>
2023-07-31 07:15:42 +0300

    IOS-3884: Improve naming and project structure

---
+++ Tangem/Modules/OrganizeTokens/AnimatableModifiers/OrganizeTokensCornerRadiusAnimatableModifier.swift
@@ -0,0 +1,16 @@
+//
+//  OrganizeTokensCornerRadiusAnimatableModifier.swift
+//  Tangem
+//
+//  Created by Andrey Fedorov on 31.07.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import SwiftUI
+
+struct OrganizeTokensCornerRadiusAnimatableModifier: AnimatableModifier {
+    func body(content: Content) -> some View {
+        // TODO: Andrey Fedorov - Add actual implementation
+        return content
+    }
+}
