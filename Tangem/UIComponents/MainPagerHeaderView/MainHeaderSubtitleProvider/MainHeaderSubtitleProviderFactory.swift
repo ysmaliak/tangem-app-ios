cb76b69556618ece4e3d5df4631517d9a91e201c
Andrew Son <andoran90@gmail.com>
2023-07-31 17:08:14 +0500

    IOS-4118: Rename from Card to UserWallet or Main

---
+++ Tangem/UIComponents/MainPagerHeaderView/MainHeaderSubtitleProvider/MainHeaderSubtitleProviderFactory.swift
@@ -0,0 +1,19 @@
+//
+//  MainHeaderSubtitleProviderFactory.swift
+//  Tangem
+//
+//  Created by Andrew Son on 28/07/23.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+struct MainHeaderSubtitleProviderFactory {
+    func provider(for userWalletModel: UserWalletModel) -> MainHeaderSubtitleProvider {
+        guard userWalletModel.isMultiWallet else {
+            return SingleWalletMainHeaderSubtitleProvider(userWalletModel: userWalletModel, walletModel: userWalletModel.walletModelsManager.walletModels.first)
+        }
+
+        return MultiWalletMainHeaderSubtitleProvider(userWalletModel: userWalletModel)
+    }
+}
