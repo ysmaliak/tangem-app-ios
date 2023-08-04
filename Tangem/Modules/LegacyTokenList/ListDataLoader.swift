41e55f9fab0b3223842dc686a5aff50fd579d480
Andrey Chukavin <megadestroyer007@gmail.com>
2023-08-04 14:36:06 +0300

    IOS-4135 Reverted the renaming of ListDataLoader

--- Tangem/Modules/LegacyTokenList/ListDataLoader.swift
+++ Tangem/Modules/LegacyTokenList/ListDataLoader.swift
@@ -1,5 +1,5 @@
 //
-//  LegacyListDataLoader.swift
+//  ListDataLoader.swift
 //  Tangem
 //
 //  Created by Alexander Osokin on 14.04.2022.
@@ -11,7 +11,7 @@ import Combine
 import TangemSdk
 import BlockchainSdk
 
-class LegacyListDataLoader {
+class ListDataLoader {
     // MARK: Dependencies
 
     @Injected(\.tangemApiService) var tangemApiService: TangemApiService
@@ -79,7 +79,7 @@ class LegacyListDataLoader {
 
 // MARK: Private
 
-private extension LegacyListDataLoader {
+private extension ListDataLoader {
     func loadItems(_ searchText: String) -> AnyPublisher<[CoinModel], Never> {
         let searchText = searchText.trimmed()
         let requestModel = CoinsListRequestModel(
