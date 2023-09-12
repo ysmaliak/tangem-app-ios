c76ed0d39ccd3428d0e50fdfa4e56d9e782afc52
Andrey Chukavin <megadestroyer007@gmail.com>
2023-09-12 16:56:08 +0300

    IOS-4074 Copyright

--- Tangem/Modules/TokenList/CoinViewModel.swift
+++ Tangem/Modules/TokenList/CoinViewModel.swift
@@ -2,7 +2,7 @@
 //  LegacyCoinViewModel.swift
 //  Tangem
 //
-//  Created by Alexander Osokin on 18.03.2022.
+//  Created by Andrey Chukavin on 31.07.2023.
 //  Copyright © 2022 Tangem AG. All rights reserved.
 //
 
