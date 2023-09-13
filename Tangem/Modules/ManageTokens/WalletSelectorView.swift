645b8ce0ca43f3bcf41820167e9cad59c3b35220
Andrey Chukavin <megadestroyer007@gmail.com>
2023-09-13 19:01:28 +0300

    IOS-4540 Initial wallet selector changes

---
+++ Tangem/Modules/ManageTokens/WalletSelectorView.swift
@@ -0,0 +1,21 @@
+//
+//  WalletSelectorView.swift
+//  Tangem
+//
+//  Created by Andrey Chukavin on 13.09.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import SwiftUI
+
+struct WalletSelectorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WalletSelectorView_Previews: PreviewProvider {
+    static var previews: some View {
+        WalletSelectorView()
+    }
+}
