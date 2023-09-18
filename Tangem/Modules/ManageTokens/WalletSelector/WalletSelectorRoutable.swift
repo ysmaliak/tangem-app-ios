4fb5f122cf0fcd957a7f6ca7d507abbb412d089d
Andrey Chukavin <megadestroyer007@gmail.com>
2023-09-18 17:15:24 +0300

    IOS-4540 Added selector coordinator

---
+++ Tangem/Modules/ManageTokens/WalletSelector/WalletSelectorRoutable.swift
@@ -0,0 +1,13 @@
+//
+//  WalletSelectorRoutable.swift
+//  Tangem
+//
+//  Created by Andrey Chukavin on 18.09.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+protocol WalletSelectorRoutable: AnyObject {
+    func didSelectWallet(with userWalletId: Data)
+}
