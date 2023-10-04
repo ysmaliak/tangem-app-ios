980a6f443ca9bd93ff24ad7a8daefdfbd7ee6a95
Andrey Chukavin <megadestroyer007@gmail.com>
2023-10-04 14:24:42 +0300

    IOS-4563 Passing existing tokens

--- Tangem/Modules/AddCustomToken/AddCustomTokenViewModel.swift
+++ Tangem/Modules/AddCustomToken/AddCustomTokenViewModel.swift
@@ -92,7 +92,7 @@ final class AddCustomTokenViewModel: ObservableObject {
         }
 
         if let existingTokenDerivationPath {
-            // TODO
+            // TODO:
         }
 
         bind()
