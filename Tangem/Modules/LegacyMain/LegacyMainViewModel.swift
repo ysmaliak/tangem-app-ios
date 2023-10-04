acc85da2d07a4ab5331a0d8f613375da8c7f0980
Skibin Alexander <skibinalexander@gmail.com>
2023-10-04 11:22:09 +0300

    IOS-4076 add load quotes without publisher

--- Tangem/Modules/LegacyMain/LegacyMainViewModel.swift
+++ Tangem/Modules/LegacyMain/LegacyMainViewModel.swift
@@ -720,6 +720,8 @@ extension LegacyMainViewModel: LegacyMultiWalletContentViewModelOutput {
         var blockchains = cardModel.config.supportedBlockchains
         blockchains.remove(.ducatus)
 
+        // TODO: - Remove Ducutas
+
         let settings = LegacyManageTokensSettings(
             supportedBlockchains: blockchains,
             hdWalletsSupported: cardModel.config.hasFeature(.hdWallets),
