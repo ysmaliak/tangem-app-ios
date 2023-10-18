e92485c14b6b2c3b3f8f950061a60ccca75eb441
Andrey Chukavin <megadestroyer007@gmail.com>
2023-10-18 18:45:31 +0300

    IOS-4793 Unlocking wallets analytics

--- Tangem/Modules/Main/UnlockCardBottomSheet/UnlockUserWalletBottomSheetViewModel.swift
+++ Tangem/Modules/Main/UnlockCardBottomSheet/UnlockUserWalletBottomSheetViewModel.swift
@@ -32,8 +32,7 @@ class UnlockUserWalletBottomSheetViewModel: ObservableObject, Identifiable {
     }
 
     func unlockWithBiometry() {
-        // TODO: Update anal
-        //        Analytics.log(.buttonUnlockAllWithFaceID)
+        Analytics.log(.buttonUnlockAllWithFaceID)
 
         userWalletRepository.unlock(with: .biometry) { [weak self] result in
             switch result {
