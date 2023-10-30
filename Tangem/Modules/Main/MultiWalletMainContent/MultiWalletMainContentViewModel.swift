b1dddf517de9c9988ac0d84c99c2c620f04b97eb
Skibin Alexander <skibinalexander@gmail.com>
2023-10-30 09:46:41 +0300

    IOS-4899 Add send event parameter

--- Tangem/Modules/Main/MultiWalletMainContent/MultiWalletMainContentViewModel.swift
+++ Tangem/Modules/Main/MultiWalletMainContent/MultiWalletMainContentViewModel.swift
@@ -353,9 +353,10 @@ private extension MultiWalletMainContentViewModel {
     }
 
     func hideToken(tokenItem: TokenItem, blockchainNetwork: BlockchainNetwork) {
-        // TODO: Add analytics
         let derivation = blockchainNetwork.derivationPath
         userWalletModel.userTokensManager.remove(tokenItem, derivationPath: derivation)
+        
+        Analytics.log(event: .buttonRemoveToken, params: [Analytics.ParameterKey.token: tokenItem.currencySymbol])
     }
 }
 
