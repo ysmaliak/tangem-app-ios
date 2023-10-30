eeea151813d302cf5c5aba4e650b04ff62174c19
Andrey Chukavin <megadestroyer007@gmail.com>
2023-10-30 12:37:41 +0300

    IOS-4973 Added sandbox send project

---
+++ Tangem/Modules/Send/Send/Send/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Send
+//
+//  Created by Andrey Chukavin on 30.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tangem/Modules/Send/Send/Send/SendApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendApp.swift
+//  Send
+//
+//  Created by Andrey Chukavin on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
