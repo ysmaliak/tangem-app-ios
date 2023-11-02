d725bc2cbd7b0c716eb22dd9b9f9dc14b89cc760
Andrey Chukavin <megadestroyer007@gmail.com>
2023-11-02 12:32:41 +0300

    IOS-4973 Moving things around

---
+++ Tangem/Preview Content/Mocks/Send/SendRoutableMock.swift
@@ -0,0 +1,13 @@
+//
+//  SendRoutableMock.swift
+//  Tangem
+//
+//  Created by Andrey Chukavin on 02.11.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+class SendRoutableMock: SendRoutable {
+    init() {}
+}
