7c95c396660c0565e8e23fdc455df75a31e56616
Skibin Alexander <skibinalexander@gmail.com>
2023-11-10 10:55:52 +0300

    IOS-5005 Remove comments

--- Tangem/Modules/Main/MainBottomSheet/Navigation/MainBottomSheetCoordinator.swift
+++ Tangem/Modules/Main/MainBottomSheet/Navigation/MainBottomSheetCoordinator.swift
@@ -51,9 +51,7 @@ class MainBottomSheetCoordinator: CoordinatorObject {
         bind()
     }
 
-    func start(with options: Options = .init()) {
-        // TODO: Andrey Fedorov - Add actual implementation
-    }
+    func start(with options: Options = .init()) {}
 
     private func bind() {
         let bottomSheetVisibilityPublisher = bottomSheetVisibility
