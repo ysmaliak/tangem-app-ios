f4731ea215e4ef204713a49302c59d6fd70523d0
Sergey Balashov <Balashov.152@gmail.com>
2023-11-10 20:27:13 +0000

    IOS-4985 Add CommonExpressManager

---
+++ TangemSwapping/Manager/ExpressManager/ExpressManagerSwappingPairRequest.swift
@@ -0,0 +1,14 @@
+//
+//  ExpressManagerSwappingPairRequest.swift
+//  TangemSwapping
+//
+//  Created by Sergey Balashov on 10.11.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+public struct ExpressManagerSwappingPairRequest {
+    let pair: ExpressManagerSwappingPair
+    let amount: Decimal
+}
