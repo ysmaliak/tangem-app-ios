1957624ac2c5adc2013e979c07ec338715f0a0d2
Andrey Chukavin <megadestroyer007@gmail.com>
2023-11-21 18:17:18 +0300

    IOS-5197 Added send address service

---
+++ Tangem/Modules/Send/SendAddressServiceFactory.swift
@@ -0,0 +1,26 @@
+//
+//  SendAddressServiceFactory.swift
+//  Tangem
+//
+//  Created by Andrey Chukavin on 21.11.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+import BlockchainSdk
+
+struct SendAddressServiceFactory {
+    private let walletModel: WalletModel
+
+    init(walletModel: WalletModel) {
+        self.walletModel = walletModel
+    }
+
+    func build() -> SendAddressService {
+        if let addressResolver = walletModel.addressResolver {
+            return SendResolvedAddressService(walletModel: walletModel, addressResolver: addressResolver)
+        } else {
+            return DefaultSendAddressService(walletModel: walletModel)
+        }
+    }
+}
