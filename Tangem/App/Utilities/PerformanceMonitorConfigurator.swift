96998f9116b7c8aba6542437468f6ee5772aed37
Andrey Fedorov <m3g0byt3@gmail.com>
2023-12-01 11:21:27 +0300

    IOS-5278: Conditionally enable performance monitor using feature toggle

--- Tangem/App/Utilities/PerformanceMonitorConfigurator.swift
+++ Tangem/App/Utilities/PerformanceMonitorConfigurator.swift
@@ -29,13 +29,13 @@ enum PerformanceMonitorConfigurator {
         return UserDefaults.standard.bool(forKey: "com.tangem.PerformanceMonitorEnabled")
     }
 
-    private static var isEnabledUsingEnvironment: Bool {
-        return false // TODO: Andrey Fedorov - Add actual implementation
+    private static var isEnabledUsingFeatureToggle: Bool {
+        return FeatureStorage().isPerformanceMonitorEnabled
     }
 
     static func configureIfAvailable() {
         #if canImport(GDPerformanceView_Swift)
-        guard isEnabledUsingLaunchArguments || isEnabledUsingEnvironment else { return }
+        guard isEnabledUsingLaunchArguments || isEnabledUsingFeatureToggle else { return }
 
         PerformanceMonitor.shared().performanceViewConfigurator.options = [.performance, .device, .system, .memory]
         PerformanceMonitor.shared().performanceViewConfigurator.style = performanceMonitorStyle
