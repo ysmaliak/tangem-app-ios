34b48a95352323559dc8ed1f19b6886466505d25
Andrey Fedorov <m3g0byt3@gmail.com>
2023-12-11 22:39:35 +0300

    IOS-4838: Implement UI for rate app bottom sheet

---
+++ Tangem/Modules/Main/RateAppBottomSheet/RateAppBottomSheetViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RateAppBottomSheetViewModel.swift
+//  Tangem
+//
+//  Created by Andrey Fedorov on 09.12.2023.
+//  Copyright © 2023 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+final class RateAppBottomSheetViewModel: ObservableObject, Identifiable {
+    // TODO: Andrey Fedorov - Add actual implementation
+}
