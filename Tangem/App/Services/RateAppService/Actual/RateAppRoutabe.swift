d0ed572a11006f32e422b5897ef990169bc1376e
Andrey Fedorov <m3g0byt3@gmail.com>
2024-01-22 17:21:26 +0300

    IOS-4838: Introduce `RateAppController` for reusing shared 'rate app' logic

---
+++ Tangem/App/Services/RateAppService/Actual/RateAppRoutabe.swift
@@ -0,0 +1,15 @@
+//
+//  RateAppRoutabe.swift
+//  Tangem
+//
+//  Created by Andrey Fedorov on 22.01.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+protocol RateAppRoutabe: AnyObject {
+    func openAppRateDialog(with viewModel: RateAppBottomSheetViewModel)
+    func openFeedbackMail(with dataCollector: EmailDataCollector, emailType: EmailType, recipient: String)
+    func openAppStoreReview()
+}
