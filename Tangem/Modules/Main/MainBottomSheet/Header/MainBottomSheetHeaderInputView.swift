ec15647254498ed64281ba75d031d1fdbe1764f4
Skibin Alexander <skibinalexander@gmail.com>
2024-02-26 11:05:41 +0300

    IOS-5815 Fix pixel perfect changes

--- Tangem/Modules/Main/MainBottomSheet/Header/MainBottomSheetHeaderInputView.swift
+++ Tangem/Modules/Main/MainBottomSheet/Header/MainBottomSheetHeaderInputView.swift
@@ -25,11 +25,12 @@ struct MainBottomSheetHeaderInputView: View {
     private var searchBar: some View {
         CustomSearchBar(
             searchText: $searchText,
-            placeholder: Localization.commonSearch
+            placeholder: Localization.manageTokensSearchPlaceholder
         )
         .allowsHitTesting(allowsHitTestingForTextField)
         .padding(.top, Constants.verticalInset)
-        .padding(.bottom, max(UIApplication.safeAreaInsets.bottom, Constants.verticalInset))
+        // TODO: - Subscribe on publisher
+//        .padding(.bottom, max(UIApplication.safeAreaInsets.bottom, Constants.verticalInset))
         .background(Colors.Background.primary)
     }
 }
