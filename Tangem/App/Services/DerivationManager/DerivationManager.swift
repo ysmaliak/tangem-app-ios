e749385467b686ce4048982bf7961954c010399b
Alexander Osokin <tureckiy@gmail.com>
2024-03-04 14:48:19 +0000

    Revert "Split card interactor"
    This reverts commit ba3aa4693c1ff8e46b9e26b003b39fcece46ebfa.

--- Tangem/App/Services/DerivationManager/DerivationManager.swift
+++ Tangem/App/Services/DerivationManager/DerivationManager.swift
@@ -14,7 +14,7 @@ protocol DerivationManager {
     var hasPendingDerivations: AnyPublisher<Bool, Never> { get }
     var pendingDerivationsCount: AnyPublisher<Int, Never> { get }
 
-    func deriveKeys(cardInteractor: KeysDeriving, completion: @escaping (Result<Void, TangemSdkError>) -> Void)
+    func deriveKeys(cardInteractor: CardDerivable, completion: @escaping (Result<Void, TangemSdkError>) -> Void)
 }
 
 // TODO: refactor keys storage to single source and remove this delegate
