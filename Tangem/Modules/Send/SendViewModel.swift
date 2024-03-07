cb6816292d84ab472b196f9bcdaac6397f7f2c4c
Andrey Fedorov <afedorov@tangem.com>
2024-03-07 20:04:48 +0300

    IOS-5604: Add support for BIP-0021 and ERC-681 to `QRCodeParser`
    Signed-off-by: Andrey Fedorov <afedorov@tangem.com>

--- Tangem/Modules/Send/SendViewModel.swift
+++ Tangem/Modules/Send/SendViewModel.swift
@@ -364,7 +364,11 @@ final class SendViewModel: ObservableObject {
 
     private func parseQRCode(_ code: String) {
         #warning("TODO: Add the necessary UI warnings")
-        let parser = QRCodeParser(amountType: walletModel.amountType, blockchain: walletModel.blockchainNetwork.blockchain)
+        let parser = QRCodeParser(
+            amountType: walletModel.amountType,
+            blockchain: walletModel.blockchainNetwork.blockchain,
+            decimalCount: walletModel.decimalCount
+        )
         let result = parser.parse(code)
 
         sendModel.setDestination(SendAddress(value: result.destination, inputSource: .qrCode))
