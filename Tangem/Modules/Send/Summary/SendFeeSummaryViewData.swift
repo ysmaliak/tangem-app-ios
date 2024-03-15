c5133b8b3b2f4ea6d8ffce71566ac6550f37883b
Andrey Chukavin <megadestroyer007@gmail.com>
2024-03-15 10:15:04 +0800

    IOS-6208 Initial changes -- duplicated the old summary UI elements

---
+++ Tangem/Modules/Send/Summary/SendFeeSummaryViewData.swift
@@ -0,0 +1,16 @@
+//
+//  SendFeeSummaryViewData.swift
+//  Tangem
+//
+//  Created by Andrey Chukavin on 15.03.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+struct SendFeeSummaryViewData: Identifiable {
+    let id = UUID()
+
+    let title: String
+    let text: String
+}
