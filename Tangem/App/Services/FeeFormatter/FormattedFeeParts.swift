edb039792b833625f33216e2dd215ef9b099d261
Andrey Chukavin <megadestroyer007@gmail.com>
2024-03-15 19:56:34 +0800

    IOS-6208 Introduced formatted fee parts

---
+++ Tangem/App/Services/FeeFormatter/FormattedFeeParts.swift
@@ -0,0 +1,14 @@
+//
+//  FormattedFeeParts.swift
+//  Tangem
+//
+//  Created by Andrey Chukavin on 15.03.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+struct FormattedFeeParts {
+    let cryptoFee: String
+    let fiatFee: String?
+}
