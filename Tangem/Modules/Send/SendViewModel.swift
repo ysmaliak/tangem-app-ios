b6473217e7271f619ce91b7a677a1dce10ab28d2
Andrey Chukavin <megadestroyer007@gmail.com>
2024-03-15 09:59:37 +0800

    IOS-6208 Added subtitle

--- Tangem/Modules/Send/SendViewModel.swift
+++ Tangem/Modules/Send/SendViewModel.swift
@@ -23,6 +23,15 @@ final class SendViewModel: ObservableObject {
         step.name
     }
 
+    var hasSubtitle: Bool {
+        subtitle != nil
+    }
+
+    var subtitle: String? {
+        // TODO!
+        walletInfo.walletName
+    }
+
     var showNavigationButtons: Bool {
         step.hasNavigationButtons
     }
