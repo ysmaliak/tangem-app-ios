ab2e2a3d18848f15a11bf91b5d5590cb9368c906
Andrey Fedorov <afedorov@tangem.com>
2024-04-02 11:04:36 +0300

    IOS-6263: Add TODO
    Signed-off-by: Andrey Fedorov <afedorov@tangem.com>

--- Tangem/App/Services/Common/AppLog.swift
+++ Tangem/App/Services/Common/AppLog.swift
@@ -40,6 +40,7 @@ class AppLog {
         Log.debug(message())
     }
 
+    // TODO: Andrey Fedorov - Get rid of this method and pass file/line as arguments to `debug` (IOS-6440)
     func debugDetailed<T>(file: StaticString = #fileID, line: UInt = #line, _ message: @autoclosure () -> T) {
         Log.debug("\(file):\(line): \(message())")
     }
