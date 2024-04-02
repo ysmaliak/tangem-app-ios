46386a917f55df69546eebf7640c2f03c576c638
Andrey Chukavin <megadestroyer007@gmail.com>
2024-04-02 13:37:19 +0800

    IOS-6424 Refactoring

---
+++ Tangem/Modules/Send/Fee/CustomFeeService/CustomFeeServiceInputOutput.swift
@@ -0,0 +1,20 @@
+//
+//  CustomFeeServiceInputOutput.swift
+//  Tangem
+//
+//  Created by Andrey Chukavin on 02.04.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import BlockchainSdk
+import Combine
+
+protocol CustomFeeServiceInput: AnyObject {
+    var amountPublisher: AnyPublisher<Amount?, Never> { get }
+    var destinationPublisher: AnyPublisher<SendAddress?, Never> { get }
+    var feeValuePublisher: AnyPublisher<BlockchainSdk.Fee?, Never> { get }
+}
+
+protocol CustomFeeServiceOutput: AnyObject {
+    func setCustomFee(_ customFee: Fee?)
+}
