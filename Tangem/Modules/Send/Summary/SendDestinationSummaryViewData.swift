73e5ebd26c28d60860d113e53874720678723ebd
Andrey Chukavin <megadestroyer007@gmail.com>
2024-04-08 17:27:44 +0900

    IOS-6493 Removed additional field from summary and finish

---
+++ Tangem/Modules/Send/Summary/SendDestinationSummaryViewData.swift
@@ -0,0 +1,14 @@
+//
+//  SendDestinationSummaryViewData.swift
+//  Tangem
+//
+//  Created by Andrey Chukavin on 08.04.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+struct SendDestinationSummaryViewData: Identifiable {
+    let id = UUID()
+    let address: String
+}
