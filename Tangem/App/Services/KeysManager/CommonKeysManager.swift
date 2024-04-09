2b4ba306ea06583321ea5fcb6797fb74c55b396a
Andrey Fedorov <afedorov@tangem.com>
2024-04-10 02:29:14 +0300

    IOS-6426: Add Base blockchain support
    Signed-off-by: Andrey Fedorov <afedorov@tangem.com>

--- Tangem/App/Services/KeysManager/CommonKeysManager.swift
+++ Tangem/App/Services/KeysManager/CommonKeysManager.swift
@@ -51,7 +51,10 @@ extension CommonKeysManager: KeysManager {
             // TODO: rename to solana
             quickNodeSolanaCredentials: .init(apiKey: keys.quiknodeApiKey, subdomain: keys.quiknodeSubdomain),
             quickNodeBscCredentials: .init(apiKey: keys.bscQuiknodeApiKey, subdomain: keys.bscQuiknodeSubdomain),
-            defaultNetworkProviderConfiguration: .init(logger: .verbose, urlSessionConfiguration: .standard),
+            defaultNetworkProviderConfiguration: .init(
+                logger: UserDefaults.standard.bool(forKey: "com.tangem.NetworkLoggingEnabled") ? .verbose : .none,
+                urlSessionConfiguration: .standard
+            ),
             networkProviderConfigurations: [:]
         )
     }
