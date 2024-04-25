f6d831a40c131b541f5de82193523bf5144091b4
Andrey Chukavin <megadestroyer007@gmail.com>
2024-04-25 15:26:30 +0900

    IOS-6551 Refactoring

---
+++ Tangem/Modules/Send/Destination/AddressView/SendDestinationTextViewInputFieldModel.swift
@@ -0,0 +1,16 @@
+//
+//  SendDestinationTextViewInputFieldModel.swift
+//  Tangem
+//
+//  Created by Andrey Chukavin on 25.04.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+// TODO: Add a comment
+
+class SendDestinationTextViewInputFieldModel: ObservableObject {
+    @Published var height: CGFloat = 10
+}
