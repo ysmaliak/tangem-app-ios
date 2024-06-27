19ec4a375312ee7baabc714ce836afed2031975b
Andrew Son <24321494+Andoran90@users.noreply.github.com>
2024-06-27 17:29:51 +0500

    IOS-6562 token markets detail screen (#3460)

---
+++ Tangem/Modules/Markets/TokenMarketsDetails/TokenMarketsDetailsCoordinator.swift
@@ -0,0 +1,36 @@
+//
+//  TokenMarketsDetailsCoordinator.swift
+//  Tangem
+//
+//  Created by Andrew Son on 24/06/24.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+class TokenMarketsDetailsCoordinator: CoordinatorObject {
+    let dismissAction: Action<Void>
+    let popToRootAction: Action<PopToRootOptions>
+
+    @Published var rootViewModel: TokenMarketsDetailsViewModel? = nil
+
+    required init(
+        dismissAction: @escaping Action<Void>,
+        popToRootAction: @escaping Action<PopToRootOptions>
+    ) {
+        self.dismissAction = dismissAction
+        self.popToRootAction = popToRootAction
+    }
+
+    func start(with options: Options) {
+        rootViewModel = .init(tokenInfo: options.info)
+    }
+}
+
+extension TokenMarketsDetailsCoordinator {
+    struct Options {
+        let info: MarketsTokenModel
+    }
+}
+
+extension TokenMarketsDetailsCoordinator: TokenMarketsDetailsRoutable {}
---
+++ Tangem/Modules/Markets/TokenMarketsDetails/TokenMarketsDetailsRoutable.swift
@@ -0,0 +1,11 @@
+//
+//  TokenMarketsDetailsRoutable.swift
+//  Tangem
+//
+//  Created by Andrew Son on 24/06/24.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+protocol TokenMarketsDetailsRoutable: AnyObject {}
